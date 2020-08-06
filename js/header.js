8ebf52f21334ca311335ae9270c86ff9ad52719a
pikten11 <66857832+pikten11@users.noreply.github.com>
2020-08-06 16:16:21 +0900

    Update header.js

--- js/header.js
+++ js/header.js
@@ -2,10 +2,10 @@ function header(){
 var html = "";
 	html += '<div class="header">';
 	html += '<div class="container">';
-	html += '<div class="header-right">';
+	html += '<div class="header-left">';
 	html += '<img class="header-logo" src="https://pikten11.github.io/gorakuba/img/gorakuba.png" alt="ゴラクバ">';
 	html += '</div>';
-	html += '<div class="header-left">';
+	html += '<div class="header-right">';
 	html += '<a href="https://pikten11.github.io/blog/index.html">トップページ</a>';
 	html += '<a href="https://pikten11.github.io/blog/form.html">お問い合わせ</a>';
 	html += '</div>';
